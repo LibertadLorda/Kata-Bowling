706ede3279445c83d5ea01f1f103b1933eff95d9
LibertadLorda <libertadlorda@gmail.com>
2023-07-25 11:21:02 +0200

    Add code and test

--- src/main/java/org/example/game/Game.java
+++ src/main/java/org/example/game/Game.java
@@ -1,6 +1,47 @@
-package org.example.bowling;
+package org.example.game;
 
-public class Bowling {
+public class Game {
 
+    private final int [] rolls = new int [21];
 
+    private int currentRoll =0;
+
+    private boolean strike(int index){
+        return rolls[index] == 10;
+    }
+
+    private boolean spare(int index){
+        return rolls[index] + rolls[index+1] == 10;
+    }
+
+    public void Roll(int pins){
+        if(currentRoll <21) {
+            rolls[currentRoll++] = pins;
+        }
+    }
+
+    private int rollIndex(int... currentIndex) {
+        int sum = 0;
+        for (int i = 0; i < currentIndex.length; i++) {
+            sum += rolls[currentIndex[i]];
+        }
+        return sum;
+    }
+
+    public int Score() {
+        int score=0;
+        int index=0;
+        for (int i = 0; i < 10; i++) {
+            score += rollIndex(index++);
+            if (strike(index -1)){
+                score += rollIndex(index, index + 1);
+            }else {
+                score += rollIndex(index++);
+                if (spare(index -2)) {
+                    score += rollIndex(index);
+                }
+            }
+        }
+        return score;
+    }
 }
--- src/test/java/org/example/game/GameTest.java
+++ src/test/java/org/example/game/GameTest.java
@@ -1,15 +1,59 @@
-package org.example.bowling;
+package org.example.game;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+class GameTest {
 
-class BowlingTest {
+    Game game = new Game();
 
-    Bowling bowling = new Bowling();
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            game.Roll(pins);
+        }
+    }
+
+    private void rollSpare() {
+        game.Roll(5);
+        game.Roll(5);
+    }
+
+    private void rollStrike() {
+        game.Roll(10);
+    }
 
     @Test
-    public void (){
-        assertEqual(
+    public void Game_with_pins_equal_0() {
+        rollMany(20, 0);
+        assertEquals(0, game.Score());
+    }
+
+    @Test
+    public void All_Ones() {
+        rollMany(20, 1);
+        assertEquals(20, game.Score());
+    }
+
+    @Test
+    public void Spare_down_10_on_2_tries_with_extra_ball() {
+        rollSpare();
+        game.Roll(3);
+        rollMany(17, 0);
+        assertEquals(16, game.Score());
+    }
+
+    @Test
+    public void Strike_down_10_with_extra_balls() {
+        rollStrike();
+        game.Roll(3);
+        game.Roll(4);
+        rollMany(16, 0);
+        assertEquals(24, game.Score());
+    }
+
+    @Test
+    public void Perfect_game() {
+        rollMany(12, 10);
+        assertEquals(300, game.Score());
     }
 }
